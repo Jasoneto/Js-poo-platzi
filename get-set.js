4d1b708b7aff8a7485639c29f7ea0411a555784c
Jason Balamb <jasonst.106@gmail.com>
2023-05-08 16:47:33 -0500

    A new example of Get and Set aplication case

--- get-set.js
+++ get-set.js
@@ -30,3 +30,41 @@ obj.prop = 12;
 
 var atr = Object.getOwnPropertyDescriptor(obj, 'prop')
 console.log(atr);
+
+//otro ejercicio Get y set
+
+export class Course {
+  constructor({
+    name,
+    classes = [],
+  }) {
+    this._name = name;
+    this.classes = classes;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(nuevoNombrecito) {
+      if (typeof nuevoNombrecito === "string") {
+      const newName = nuevoNombrecito
+      const palabras = newName.split(" ");
+
+      const word = palabras.map((palabra) => {
+        return palabra[0].toUpperCase() + palabra.substring(1);
+      }).join(" ")
+      // console.log(palabras);
+      console.log(word);
+      this._name = word;
+    } else {
+      console.error("no se puede cambiar el nombre")
+    }
+  }
+}
+
+const courseName = "curso de programación básica";
+const nombreMaysuculas = new Course({
+  name: courseName,
+})
+nombreMaysuculas.name = courseName;
