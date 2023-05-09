b26757ef2c4bc630731e137b66abf11c558108bf
Jason Balamb <jasonst.106@gmail.com>
2023-05-09 11:27:57 -0500

    adding example of hereditary JS OOP

---
+++ metodo-heredado.js
@@ -0,0 +1,20 @@
+import { Comment } from "./Comment";
+import { Student } from "./Student";
+
+export class TeacherStudent extends Student{
+    constructor(props, skills=[]) {
+    super(props);
+    this.skills = skills;
+}
+
+    publicarComentario(commentContent) {
+        const role = `profesor de ${this.skills.join(",")}`;
+        //console.log(role);
+        const comment = new Comment({
+            content: commentContent,
+            studentName: this.name,
+            studentRole: role
+        });
+    return comment.publicar();
+    }
+}
